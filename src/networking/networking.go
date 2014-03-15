d17fe1fdeb826e01632d2f695ae23707e510f712
JMolvaer <joarmol@stud.ntnu.no>
2014-03-15 02:53:47 +0100

    Init update

--- src/networking/networking.go
+++ src/networking/networking.go
@@ -38,6 +38,7 @@ func GenerateMessage(dir elevator.Elev_button, floor int, inout int, state strin
 
 func SendStatuslist(generatedMsgs_c chan Networkmessage) {
     for _, status:= range statuslist {
+        fmt.Println("JA")
         generatedMsgs_c <- GenerateMessage(elevator.BUTTON_CALL_UP,0,0,status.State, status.LastFloor,false,status.Source)
         time.Sleep(100 * time.Millisecond)
     }
